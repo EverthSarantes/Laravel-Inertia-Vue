9a5307b804c527e2cfde2b4efe169f140086ac76
Everth Sarantes <everthleonardosaran@gmail.com>
2025-04-15 20:43:34 -0600

    feat: add Backup model and table form data trait for backup management

---
+++ app/Traits/TableFormData/Backup.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Traits\TableFormData;
+
+use App\Traits\TableFormData\TableFormDataTrait;
+
+/**
+ * Trait User
+ * Provides functionality for managing backup-related data in tables and forms.
+ */
+trait Backup
+{
+    use TableFormDataTrait;
+
+    /**
+     * @var array<string> Modules that the user has permission to access.
+     */
+    public static $permisson_modules = [
+        'backups',
+    ];
+
+    /**
+     * @var array<string> Fields to display in the table.
+     */
+    public static $table_fields = [
+        'name',
+    ];
+
+    /**
+     * @var array<string> Fields in the table that can be searched.
+     */
+    public static $table_fields_searchable = [
+        'name',
+    ];
+
+    /**
+     * @var array<string, string> Field names for the table.
+     */
+    public static $table_fields_names = [
+        'name' => 'Nombre',
+    ];
+
+
+    /**
+     * @var string The field name representing the model's name.
+     */
+    public static $name_field = 'name';
+
+    /**
+     * Generates options for the field content in the table.
+     *
+     * @return array<string, array<string, mixed>> The options for the field content.
+     */
+    public function getOptionsFieldContent()
+    {
+        return [
+            /* 'show' => [
+                'type' => 'link',
+                'attr' => [
+                    'href' => route('users.show', ['user' => $this]),
+                    'class' => 'btn btn-primary',
+                ],
+                'inner' => '<i class=\'bx bxs-show\'></i>',
+            ],
+            'delete' => [
+                'type' => 'button',
+                'attr' => [
+                    'class' => 'delete-button btn btn-danger',
+                    'data-url' => route('users.delete', ['user' => $this]),
+                ],
+                'inner' => '<i class=\'bx bxs-trash-alt\'></i>',
+            ], */
+        ];
+    }
+}
--- config/model.php
+++ config/model.php
@@ -4,4 +4,5 @@ return [
     'User' => \App\Models\User::class,
     'Module' => \App\Models\Users\Module::class,
     'UserModule' => \App\Models\Users\UserModule::class,
+    'Backup' => \App\Models\Backups\Backup::class,
 ];
