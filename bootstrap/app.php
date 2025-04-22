ed31d5dbbe58480a9e183f28aa3e421fbd60a724
Everth Sarantes <everthleonardosaran@gmail.com>
2025-04-22 14:44:02 -0600

    fix: add error handling for backup scheduling process

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,22 +33,27 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withSchedule(function (Schedule $schedule) {
-        ScheduledBackup::where('active', true)->get()->each(function ($backup) use ($schedule) {
-            foreach ($backup->times as $time) {
-                $schedule->call(function  () {
-                    Log::channel('backups')->info('Iniciando backup...');
-                    BackupServices::createBackup();
-                })
-                ->days($backup->days)
-                ->at($time)
-                ->name("Backup-{$backup->id}-{$time}")
-                ->evenInMaintenanceMode()
-                ->after(function (Stringable $output) {
-                    Log::channel('backups')->info('Backup Finalizado...', [
-                        'output' => $output,
-                    ]);
-                });
-            }
-        });
+        try{
+            ScheduledBackup::where('active', true)->get()->each(function ($backup) use ($schedule) {
+                foreach ($backup->times as $time) {
+                    $schedule->call(function  () {
+                        Log::channel('backups')->info('Iniciando backup...');
+                        BackupServices::createBackup();
+                    })
+                    ->days($backup->days)
+                    ->at($time)
+                    ->name("Backup-{$backup->id}-{$time}")
+                    ->evenInMaintenanceMode()
+                    ->after(function (Stringable $output) {
+                        Log::channel('backups')->info('Backup Finalizado...', [
+                            'output' => $output,
+                        ]);
+                    });
+                }
+            });
+        }
+        catch (\Exception $e) {
+            Log::error('Error scheduling backups: ' . $e->getMessage());
+        }
     })
     ->create();
