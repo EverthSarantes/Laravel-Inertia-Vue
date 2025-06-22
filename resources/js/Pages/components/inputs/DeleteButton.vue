522abd9d7ed2dbc074d6f45dc7d4e075182e9002
Everth Sarantes <everthleonardosaran@gmail.com>
2025-06-22 13:43:09 -0600

    feat: add DeleteButton component for modal delete functionality

---
+++ resources/js/Pages/components/inputs/DeleteButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+    defineProps({
+        url: String,
+        sm: {
+            type: Boolean,
+            default: false
+        }
+    });
+
+    function showDeleteModal(url) {
+        document.querySelector('#delete_modal form').action = url;
+        let modalInstance = new bootstrap.Modal(document.getElementById('delete_modal'));
+        modalInstance.show();
+    }
+</script>
+
+<template>
+    <button class="btn btn-danger" @click="showDeleteModal(url)" :class="{'btn-sm': sm}">
+        <i class='bx bx-trash'></i>
+    </button>
+</template>
