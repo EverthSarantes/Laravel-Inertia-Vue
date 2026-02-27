9c2db36cb1903e278af0cae12264bc10cae67094
Everth <everthleonardosaran@gmail.com>
2026-02-27 09:47:49 -0600

    feat(FormattedDate): add component for formatted date display with customizable format

---
+++ resources/js/Pages/components/accounting/FormattedDate.vue
@@ -0,0 +1,37 @@
+<script setup>
+    import { computed } from 'vue';
+
+    const props = defineProps({
+        date: String,
+        date_format: {
+            type: String,
+            default: 'd/m/Y',
+        },
+    });
+
+    const formattedValue = computed(() => {
+        if (!props.date) {
+            return '';
+        }
+        const format = props.date_format || 'd/m/Y';
+
+        const [year, month, day] = props.date.split('-');
+
+        if (!year || !month || !day) {
+            return props.date;
+        }
+
+        const formattedDate = format
+            .replace('d', day)
+            .replace('m', month)
+            .replace('Y', year);
+
+        return formattedDate;
+    });
+</script>
+
+<template>
+    <span>
+        {{ formattedValue }}
+    </span>
+</template>
