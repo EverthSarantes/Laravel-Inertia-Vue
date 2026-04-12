f9dcc647c5d5bfcc24a2b29872e9218f6158a967
Everth Sarantes <everthleonardosaran@gmail.com>
2026-04-12 14:05:05 -0600

    fix(tabs): remove active class from modules tab for consistent styling

--- resources/js/Pages/administration_app/users_templates/show.vue
+++ resources/js/Pages/administration_app/users_templates/show.vue
@@ -235,7 +235,7 @@
         <div class="container p-3">
             <ul class="nav nav-tabs flex-row justify-content-end" id="pills-tab" role="tablist" style="border: none;">
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="pills-modules-tab" data-bs-toggle="pill" data-bs-target="#pills-modules" type="button" role="tab" aria-controls="pills-modules" aria-selected="true">Modulos</button>
+                    <button class="nav-link" id="pills-modules-tab" data-bs-toggle="pill" data-bs-target="#pills-modules" type="button" role="tab" aria-controls="pills-modules" aria-selected="true">Modulos</button>
                 </li>
                 <li class="nav-item" role="presentation">
                     <button class="nav-link" id="pills-model-filters-tab" data-bs-toggle="pill" data-bs-target="#pills-model-filters" type="button" role="tab" aria-controls="pills-model-filters" aria-selected="false">Filtros de Información</button>
@@ -244,7 +244,7 @@
         </div>
 
         <div class="tab-content container" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="pills-modules" role="tabpanel" aria-labelledby="pills-modules-tab">
+            <div class="tab-pane fade" id="pills-modules" role="tabpanel" aria-labelledby="pills-modules-tab">
                 <div class="row">
                     <div class="col-lg-12 mt-2">
                         <div class="d-flex justify-content-between align-items-center">
