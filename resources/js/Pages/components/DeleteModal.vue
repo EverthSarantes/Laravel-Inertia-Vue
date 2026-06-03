c06e4414b00023a46fa1a1646c52b7e32d7553a8
Everth <everthleonardosaran@gmail.com>
2026-06-03 15:22:36 -0600

    change delete modal to tailwind

--- resources/js/Pages/components/DeleteModal.vue
+++ resources/js/Pages/components/DeleteModal.vue
@@ -2,6 +2,8 @@
 // It uses Inertia.js for form handling and Bootstrap for modal functionality.
 <script setup>
     import { useForm } from '@inertiajs/vue3';
+    import { ref } from 'vue';
+    import Modal from './Modal.vue';
 
     const props = defineProps({
         callback: {
@@ -15,6 +17,8 @@
         _method: 'DELETE',
     });
 
+    const modalRef = ref(null);
+    
     const submit = () => {
         let form = document.getElementById('deleteForm');
         let delete_url = form.action;
@@ -22,8 +26,7 @@
         form_request.delete(delete_url, {
             onSuccess: (response) => {
                 form.reset();
-                const modal = bootstrap.Modal.getInstance(document.getElementById('delete_modal'));
-                modal.hide();
+                closeDeleteModal();
 
                 if(window.atm_tables){
                     window.atm_tables.forEach((table) => {
@@ -37,30 +40,31 @@
             },
             onError: () => {
                 showToast('Error al eliminar el registro');
-                const modal = bootstrap.Modal.getInstance(document.getElementById('delete_modal'));
-                modal.hide();
+                closeDeleteModal();
             },
         });
     }
 
+    function openDeleteModal(url) {
+        const form = document.getElementById('deleteForm');
+        form.action = url;
+        modalRef.value.openModal();
+    }
+
+    function closeDeleteModal() {
+        const form = document.getElementById('deleteForm');
+        form.action = '';
+        modalRef.value.closeModal();
+    }
+
+    defineExpose({
+        openDeleteModal,
+        closeDeleteModal,
+    });
 </script>
 
 <template>
-    <div>
-        <div class="modal fade" id="delete_modal" tabindex="-1" aria-labelledby="delete_modal_label" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="delete_modal_label">¿Está Seguro de que desea eliminar este registro?</h1>
-                    </div>
-                    <form id="deleteForm" action="" method="POST" class="d-none">
-                    </form>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                        <button type="button" class="btn btn-danger" @click="submit">Eliminar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <Modal :title="'¿Está Seguro de que desea eliminar este registro?'" :id="'delete_modal'" ref="modalRef" :accept-callback="submit">
+        <form id="deleteForm" action="" method="POST" class="d-none"></form>
+    </Modal>
 </template>
