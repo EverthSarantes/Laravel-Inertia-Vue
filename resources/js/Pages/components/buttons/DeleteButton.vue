7f9361f826d232a925288930c359a40e87e0886e
Everth Sarantes <everthleonardosaran@gmail.com>
2026-06-07 12:09:10 -0600

    update delete button

--- resources/js/Pages/components/buttons/DeleteButton.vue
+++ resources/js/Pages/components/buttons/DeleteButton.vue
@@ -1,23 +1,25 @@
 <script setup>
-    defineProps({
+    const props = defineProps({
         url: String,
+        deleteModal:{
+            type: Object
+        },
         sm: {
             type: Boolean,
             default: false
         }
     });
 
-    function showDeleteModal(url) {
-        document.querySelector('#delete_modal form').action = url;
-        let modalInstance = new bootstrap.Modal(document.getElementById('delete_modal'));
-        modalInstance.show();
+    function showDeleteModal() {
+        props.deleteModal.openDeleteModal(props.url);
     }
 </script>
 
 <template>
     <button aria-label="eliminar"
-        class="btn btn-danger"
-        @click="showDeleteModal(url)" 
+        type="button"
+        class="min-w-[35px] h-[35px] inline-flex items-center justify-center bg-red-600 hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-600 text-white rounded transition-colors"
+        @click="showDeleteModal()" 
         :class="{'btn-sm': sm}">
         <i class='bx bx-trash'></i>
     </button>
