07b76fe72769f216a42f3b6fbc823bffefccfb4c
Everth Sarantes <everthleonardosaran@gmail.com>
2026-06-07 13:10:50 -0600

    change bootstrap to tailwind in user template index view

--- resources/js/Pages/administration_app/users_templates/index.vue
+++ resources/js/Pages/administration_app/users_templates/index.vue
@@ -8,49 +8,44 @@
     import Table from '../../components/Table.vue';
     import Form from '../../components/Form.vue';
     import ExcelExport from '../../components/buttons/ExcelExport.vue';
-
+    import Modal from '../../components/Modal.vue';
     import { usePage } from '@inertiajs/vue3';
+    import { ref } from 'vue';
     
     const links = [
         { route: 'users.index', name: 'Usuarios', active: false },
         { route: 'users.templates.index', name: 'Plantillas de Usuarios', active: true },
     ];
+
+    const modalRef = ref(null);
+    const addTemplateFormRef = ref(null);
+
+    function callback(){
+        addTemplateFormRef.value.submitFormData();
+    }
 </script>
 
 <template>
     <dashboard :appName="'administration_app'">
         <SubNavbar :links="links" />
-        <div class="container">
-            <div class="modal fade" tabindex="-1" id="AddUserModal">
-                <div class="modal-dialog modal-lg modal-dialog-scrollable">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Crear Plantilla Usuario</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
-                        </div>
-                        <div class="modal-body">
-                            <Form :route="route('users.templates.store')" :method="'POST'" :model="usePage().props.model" :is-modal="true"></Form>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="container px-4 pt-4">
+            <Modal :title="'Crear Plantilla Usuario'" :id="'AddTemplateModal'" ref="modalRef" :accept-callback="callback">
+                <Form :route="route('users.templates.store')" :method="'POST'" :model="usePage().props.model" :is-modal="modalRef" :show-submit-button="false" ref="addTemplateFormRef"></Form>
+            </Modal>
 
-            <div class="row">
-                <div class="col-lg-12 mt-4">
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h5>Plantillas de Usuarios</h5>
-                        <div class="d-flex gap-1">
-                            <ExcelExport :filename="'Plantillas de Usuarios'" :target="'users_table'"/>
-                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#AddUserModal" aria-label="Agregar Plantilla Usuario">
-                                <i class='bx bx-plus'></i>
-                            </button>
-                        </div>
+            <div class="w-full mt-4">
+                <div class="flex flex-col sm:flex-row justify-between items-center mb-4 gap-4 sm:gap-0">
+                    <h5 class="text-xl font-semibold text-gray-900 dark:text-gray-100">Plantillas de Usuarios</h5>
+                    <div class="flex items-center gap-2">
+                        <ExcelExport :filename="'Plantillas de Usuarios'" :target="'users_table'"/>
+                        <button type="button" class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded transition-colors"
+                            @click="modalRef.openModal()"
+                            aria-label="Agregar Plantilla Usuario">
+                            <i class='bx bx-plus'></i>
+                        </button>
                     </div>
-                    <Table :model="usePage().props.model" :options="['delete']" :id="'users'"/>
                 </div>
+                <Table :model="usePage().props.model" :options="['delete']" :id="'users'"/>
             </div>
         </div>
         <DeleteModal />
